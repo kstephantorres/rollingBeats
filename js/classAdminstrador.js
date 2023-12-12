1e85274ccc957d463e2608880d502794ec294dc1
K Stephan Torres <kevinstephantorres@gmail.com>
2023-12-12 00:48:45 -0300

    se creao clase Admin

---
+++ js/classAdminstrador.js
@@ -0,0 +1,54 @@
+class Admin {
+    
+    #id
+    #usuario
+    #email
+    #contrasenia
+
+    constructor(usuario, email, contrasenia) {
+        this.#id = crypto.randomUUID();
+        this.#usuario = usuario;
+        this.#email = email;
+        this.#contrasenia = contrasenia;
+    }
+
+    // Getter y Setter para id
+    get id() {
+        return this.#id;
+    }
+
+    // Getter y Setter para usuario
+    get usuario() {
+        return this.#usuario;
+    }
+
+    set usuario(value) {
+        this.#usuario = value;
+    }
+
+    // Getter y Setter para email
+    get email() {
+        return this.#email;
+    }
+
+    set email(value) {
+        this.#email = value;
+    }
+
+    // Getter y Setter para contraseña
+    get contrasenia() {
+        return this.#contrasenia;
+    }
+
+    set contrasenia(value) {
+        this.#contrasenia = value;
+    }
+    toJSON(){
+        return {
+            id: this.id,
+            usuario: this.usuario,
+            email: this.email,
+            contrasenia: this.contrasenia
+        }
+    }
+}
