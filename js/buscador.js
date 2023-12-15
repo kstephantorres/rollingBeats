258e65132d75f8eb6ac723880b772eefd684e6e0
Andres Santamarina <andresesantamarina@gmail.com>
2023-12-15 19:51:21 -0300

    se agregaron las funciones buscarTexto y limpiarBuscador a buscador.js

--- js/buscador.js
+++ js/buscador.js
@@ -1,3 +1,32 @@
 import {canciones} from "./app.js"
 import { actualizarLista } from "./app.js";
 
+const formularioBuscador = document.querySelector('form');
+const sinResultadosMensaje = document.getElementById('sinResultados');
+const contenedorLista = document.querySelector('.contenedorLista');
+
+function buscarTexto(e){
+    e.preventDefault();
+    const inputBuscador = document.querySelector('.buscador');
+    const terminoBusqueda = inputBuscador.value.toLowerCase();
+    const resultados = canciones.filter(cancion =>
+        cancion.titulo.toLowerCase().includes(terminoBusqueda) ||
+        cancion.artista.toLowerCase().includes(terminoBusqueda)
+        );
+    
+    if (resultados.length === 0){
+        sinResultadosMensaje.className = 'd-block';
+        contenedorLista.className = 'd-none';
+    }else{
+        sinResultadosMensaje.className = 'd-none';
+        contenedorLista.className = 'd-block';
+        actualizarLista(resultados);
+    }
+    limpiarBuscador();
+}
+
+function limpiarBuscador(){
+    formularioBuscador.reset();
+}
+
+formularioBuscador.addEventListener('submit', buscarTexto)
