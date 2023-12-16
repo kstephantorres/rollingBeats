e149678610a4e044c8719297266ec16039fdb833
Javier Jimenez <javier.sniguel@gmail.com>
2023-12-16 16:03:57 -0300

    Se agrego el script en todas las paginas y se agrego tambien los nav de registro y nav de cerrar session

--- js/paginas.js
+++ js/paginas.js
@@ -2,12 +2,19 @@
 //ocultar el registrar e iniciar seccion
 function mostrar(){
  const navAdm = document.querySelector('#nav-adm');
+ const navRegistros = document.querySelector('#contenedorNavBoton');
+ const navCerrarSession = document.querySelector('#contenedorNavBotonAdm');
+
  const validar = JSON.parse(localStorage.getItem('log-acces'))
  if(validar === true){
-    navAdm.className = 'd-block';
+    navAdm.className = 'd-flex';
+    navRegistros.className = 'd-none';
+    navCerrarSession.className = 'd-flex';
  }
  else{
     navAdm.className = 'd-none';
+    navRegistros.className = 'd-flex';
+    navCerrarSession.className = 'd-none';
  }
 }
 
