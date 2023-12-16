d875c216c69316c70a0a705179ccadc95fb94710
Javier Jimenez <javier.sniguel@gmail.com>
2023-12-16 16:20:51 -0300

    Se creo la funcion cerrarSesion en la pagina de adiministrador se agrego una nueva clase para que este funcionara

--- js/paginas.js
+++ js/paginas.js
@@ -1,11 +1,13 @@
  
+const btnSalir = document.querySelector('#boton_salir')
+btnSalir.addEventListener('click', cerrarSesion);
+const validar = JSON.parse(localStorage.getItem('log-acces'))
 //ocultar el registrar e iniciar seccion
 function mostrar(){
  const navAdm = document.querySelector('#nav-adm');
  const navRegistros = document.querySelector('#contenedorNavBoton');
  const navCerrarSession = document.querySelector('#contenedorNavBotonAdm');
 
- const validar = JSON.parse(localStorage.getItem('log-acces'))
  if(validar === true){
     navAdm.className = 'd-flex';
     navRegistros.className = 'd-none';
@@ -17,5 +19,13 @@ function mostrar(){
     navCerrarSession.className = 'd-none';
  }
 }
+
+function cerrarSesion(){
+    alert('Estas cerrando sessión');
+    localStorage.removeItem('log-acces');
+    window.location.href = 'login.html'
+}
+
+
 
 mostrar();
