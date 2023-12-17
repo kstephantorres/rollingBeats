96ce3eb7b568b10c2f828d290630b5a238184f5b
Andres Santamarina <andresesantamarina@gmail.com>
2023-12-16 22:06:28 -0300

    cree las funciones para ir actualizando el slider y el contador cuando se reproduce la cancion, controlar el volumen, mutear y cambiar el icono del sonido

--- js/reproductorMusica.js
+++ js/reproductorMusica.js
@@ -22,3 +22,42 @@ function mostrarDuracion() {
 function setSliderMax() {
     sliderProgresoCancion.max = Math.floor(audio.duration);
 }
+function mientrasSeReproduce() {
+    sliderProgresoCancion.value = Math.floor(audio.currentTime);
+    contenedorTiempoActual.textContent = calcularTiempo(sliderProgresoCancion.value);
+    raf = requestAnimationFrame(mientrasSeReproduce);
+}
+if (audio.readyState > 0) {
+    mostrarDuracion();
+    setSliderMax();
+} else {
+    audio.addEventListener('loadedmetadata', () => {
+        mostrarDuracion();
+        setSliderMax();
+        sliderProgresoCancion.value = Math.floor(audio.currentTime);
+        contenedorTiempoActual.textContent = calcularTiempo(sliderProgresoCancion.value);
+    });
+}
+function actualizarIconoMute() {
+    const iconoMute = '<i class="bi bi-volume-mute"></i>';
+    const iconoVolumen = '<i class="bi bi-volume-up"></i>';
+    mutear.innerHTML = audio.muted ? iconoMute : iconoVolumen;
+}
+
+function mutearVolumen() {
+    if (audio.muted) {
+        // Si está muteado, desmutar y establecer el volumen al valor actual del slider
+        audio.muted = false;
+        audio.volume = 1
+        sliderVolumen.value = 100
+        contenedorOutput.textContent = sliderVolumen.value
+        // mutear.innerHTML = iconoVolumen;
+    } else {
+        // Si no está muteado, mutar y establecer el volumen en cero
+        audio.muted = true;
+        // mutear.innerHTML = iconoMute;
+        sliderVolumen.value = 0;
+    }
+    actualizarIconoMute();
+    contenedorOutput.textContent = sliderVolumen.value;
+}
