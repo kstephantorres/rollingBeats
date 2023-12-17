5a91c04185e7ddf4b353bff1ca0a4a435e215118
Javier Jimenez <javier.sniguel@gmail.com>
2023-12-17 17:24:14 -0300

    se acomodo el codigo de tal forma que al cargar los elementos del DOM no hay pantallazos

--- js/paginas.js
+++ js/paginas.js
@@ -1,34 +1,38 @@
- 
-const btnSalir = document.querySelector('#boton_salir')
+const btnSalir = document.querySelector('#boton_salir');
+const botonRegistrar = document.querySelector('#boton_registrar');
+const navAdm = document.querySelector('#nav-adm');
+const validar = JSON.parse(localStorage.getItem('log-acces'));
+
 btnSalir.addEventListener('click', cerrarSesion);
-const validar = JSON.parse(localStorage.getItem('log-acces'))
-//ocultar el registrar e iniciar seccion
-function mostrar(){
 
- const botonRegistrar = document.querySelector('#boton_registrar');
- const navAdm = document.querySelector('#nav-adm');
- if(validar === true){
-   btnSalir.innerHTML = 'Salir';
-   botonRegistrar.innerHTML = 'Cuenta'
-   navAdm.className = 'd-flex';
-   
- }
- else{
- navAdm.className = 'd-none';
-   //  navRegistros.className = 'd-flex';
-   //  navCerrarSession.className = 'd-none';
- }
-}
-
-function cerrarSesion(){
-   if(validar==true){
-      alert('Estas cerrando sessión');
-      localStorage.removeItem('log-acces');
-      window.location.href = 'login.html'
-   }
+function mostrar() {
+  if (validar === true) {
+    navAdm.classList.add('d-flex');
+    btnSalir.style.transition = 'opacity 0.5s ease-in-out';
+    btnSalir.style.opacity = 0;
+    botonRegistrar.style.transition = 'opacity 0.5s ease-in-out';
+    botonRegistrar.style.opacity = 0;
     
+    setTimeout(() => {
+      btnSalir.textContent = 'Salir';
+      btnSalir.style.opacity = 1;
+      botonRegistrar.textContent = 'Cuenta';
+      botonRegistrar.style.opacity = 1;
+    }, 600);
+  } else {
+    navAdm.classList.add('d-none');
+  }
 }
 
+function cerrarSesion() {
+  if (validar === true) {
+    alert('Estás cerrando sesión');
+    localStorage.removeItem('log-acces');
+    window.location.href = 'login.html';
+  }
+}
 
-
-mostrar();
+// Aplicar transiciones una vez que el DOM haya cargado
+document.addEventListener('DOMContentLoaded', () => {
+  mostrar();
+});
